06173ecaf52623d18536e89724e65a6962eb212f
Szymon Kozioł <szymon.koziol@deployed.pl>
2025-03-27 18:57:21 +0100

    Validation for flashcardset pick name dialog

--- src/components/flashcard-sets/EnterFlashcardSetName.tsx
+++ src/components/flashcard-sets/EnterFlashcardSetName.tsx
@@ -1,6 +1,9 @@
 import { type ReactNode, useId, useState, useEffect } from 'react';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
 import { Button } from '@/components/base/Button';
 import {
@@ -11,7 +14,20 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/base/Dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/base/Form';
 import { Input } from '@/components/base/Input';
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: 'dialog.pickName.nameIsRequired' }),
+});
+type FormSchema = z.infer<typeof formSchema>;
 
 export type EnterFlashcardSetNameProps = {
   children: ReactNode;
@@ -20,15 +36,26 @@ export type EnterFlashcardSetNameProps = {
 
 export function EnterFlashcardSetName({ children, onSetName }: EnterFlashcardSetNameProps) {
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [name, setName] = useState('');
   const formId = useId();
   const { t } = useTranslation('sets');
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = ({ name }: FormSchema) => {
+    setDialogOpen(false);
+    onSetName?.(name);
+  };
+
   useEffect(() => {
     if (dialogOpen) {
       // Reset the name when the dialog is opened
-      setName('');
+      form.reset();
     }
-  }, [dialogOpen]);
+  }, [dialogOpen, form, form.reset]);
 
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
@@ -37,22 +64,23 @@ export function EnterFlashcardSetName({ children, onSetName }: EnterFlashcardSet
         <DialogHeader>
           <DialogTitle>{t('dialog.pickName.title')}</DialogTitle>
         </DialogHeader>
-        <form
-          id={formId}
-          onSubmit={(e) => {
-            e.preventDefault();
-            onSetName?.(name);
-            setDialogOpen(false);
-          }}
-        >
-          <Input
-            placeholder={t('dialog.pickName.placeholder')}
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-        </form>
+        <Form {...form}>
+          <form id={formId} onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t('dialog.pickName.name')}</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
 
         <DialogFooter className="flex flex-row justify-between">
           <Button variant="outlined" onClick={() => setDialogOpen(false)}>
